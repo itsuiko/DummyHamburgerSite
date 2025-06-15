6d7f103bf871606bbadc4b5c0ed86fcafccdb739
itsuikolaptop <new.life.bumper@gmail.com>
2025-06-15 09:29:44 +0900

    サイトバー完了

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 //javascript
 
 
-const menu = document.querySelector('.js-menu');
+const menu = document.querySelectorAll('.js-menu');
 const sidebar = document.querySelectorAll('.js-sidebar');
 
-menu.addEventListener('click', () => {
-    sidebar.forEach(el => el.classList.toggle('is-active'));
-});
+menu.forEach(menuItem => {
+    menuItem.addEventListener('click', () => {
+        sidebar.forEach(sidebarItem => sidebarItem.classList.toggle('is-active'));
+    });
+});    
